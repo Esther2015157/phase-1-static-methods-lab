bcd3660d8e6729b3f4f5df0f916e15f37cbbf18f
Esther2015157 <esthersly8@gmail.com>
2022-06-26 15:38:39 +0300

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,45 +1,29 @@
-describe( "Formatter", () => {
-  describe( "capitalize", () => {
-    it( "is a static method", () => {
-      expect( Formatter.capitalize( "crocodile" ) ).to.not.throw
-    } )
+class Formatter {
+  //add static methods here
+  static capitalize( string ) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
 
-    it( "uppercases the first letter in a String", () => {
-      expect( Formatter.capitalize( "a" ) ).to.equal( "A" )
-      expect( Formatter.capitalize( "alligator" ) ).to.equal( "Alligator" )
-      expect( Formatter.capitalize( "BURSTING BALLOON" ) ).to.equal( "BURSTING BALLOON" )
-    } )
-  } )
+  static sanitize( string ) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
 
-  describe( "sanitize", () => {
-    it( "is a static method", () => {
-      expect( Formatter.sanitize( "C catching colds" ) ).to.not.throw
-    } )
+  static titleize( sentence ) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let arrayOfWords = sentence.split( " " )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
+      }
 
-    it( "removes non-alphanumeric characters except for dash, single quote and space", () => {
-      expect( Formatter.sanitize( "c" ) ).to.equal( "c" )
-      expect( Formatter.sanitize( "!" ) ).to.equal( "" )
-      expect( Formatter.sanitize( "ca$@#tching cold" ) ).to.equal( "catching cold" )
-      expect( Formatter.sanitize( "Doin' Dishes" ) ).to.equal( "Doin' Dishes" )
-      expect( Formatter.sanitize( "Entert*ain(i{ng-Elephan^ts" ) ).to.equal( "Entertaining-Elephants" )
-    } )
-  } )
-
-  describe( "titleize", () => {
-    it( "is a static method", () => {
-      expect( Formatter.titleize( "F forever foolish" ) ).to.not.throw
-    } )
-
-    it( "capitalizes all words in a sentence except 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'", () => {
-      expect( Formatter.titleize( "getting giggles" ) ).to.equal( "Getting Giggles" )
-      expect( Formatter.titleize( "where the wild things are" ) ).to.equal( "Where the Wild Things Are" )
-      expect( Formatter.titleize( "chicken soup with rice and a few other songs" ) ).to.equal( "Chicken Soup With Rice and a Few Other Songs" )
-      expect( Formatter.titleize( "Maurice a an but of and for at by from end" ) ).to.equal( "Maurice a an but of and for at by from End" )
-    } )
-
-    it( "always capitalizes the first word", () => {
-      expect( Formatter.titleize( "a tale of two cities" ) ).to.equal( "A Tale of Two Cities" )
-      expect( Formatter.titleize( "in the night kitchen" ) ).to.equal( "In the Night Kitchen" )
-    } )
-  } )
-} )
+    }
+    return result.join( " " );
+  }
+} 
